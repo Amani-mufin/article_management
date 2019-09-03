5ef52bd273690d9e0ae6f1ab5da77883612d9519
smartpro1 <akeni.promise@yahoo.com>
2019-09-03 10:20:37 +0100

    implemented login and register

--- application.py
+++ application.py
@@ -2,15 +2,30 @@
 
 # from cs50 import SQL
 from flask import Flask, flash, jsonify, redirect, render_template, request, session
-# from flask_session import Session
-# from tempfile import mkdtemp
-# from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
-# from werkzeug.security import check_password_hash, generate_password_hash
+from functools import wraps
+from flask_session import Session
+from tempfile import mkdtemp
+from werkzeug.exceptions import default_exceptions, HTTPException, InternalServerError
+from werkzeug.security import check_password_hash, generate_password_hash
 
 # from helpers import apology, login_required, lookup, usd
 
+def login_required(f):
+    """
+    Decorate routes to require login.
+
+    http://flask.pocoo.org/docs/1.0/patterns/viewdecorators/
+    """
+    @wraps(f)
+    def decorated_function(*args, **kwargs):
+        if session.get("user_id") is None:
+            return redirect("/login")
+        return f(*args, **kwargs)
+    return decorated_function
+
 # Configure application
 app = Flask(__name__)
+app.config['SECRET_KEY'] = '9OLWxND4o83j4K4iuopO'
 
 # # Ensure templates are auto-reloaded
 # app.config["TEMPLATES_AUTO_RELOAD"] = True
@@ -23,9 +38,6 @@ app = Flask(__name__)
 #     response.headers["Pragma"] = "no-cache"
 #     return response
 
-
-# # Custom filter
-# app.jinja_env.filters["usd"] = usd
 
 # # Configure session to use filesystem (instead of signed cookies)
 # app.config["SESSION_FILE_DIR"] = mkdtemp()
@@ -42,151 +54,63 @@ app = Flask(__name__)
 
 
 @app.route("/")
-# @login_required
+@login_required
 def index():
     """Show portfolio of stocks"""
-    # rows = db.execute("SELECT cash FROM users WHERE id = :id", id=session["user_id"])
-    # balance = rows[0]['cash']
-    # grandtotal = balance
-    # rows = db.execute("SELECT symbol FROM tranzact WHERE user_id=:id", id=session["user_id"])
-    # symbols = []
-    # for row in rows:
-    #     symbols.append(row['symbol'])
-    # symbols = list(set(symbols))
-    # data = []
-    # print(symbols)
-    # for i in range(len(symbols)):
-    #     rows = db.execute("SELECT symbol, SUM(shares) FROM tranzact WHERE user_id=:id AND symbol=:symbol",
-    #                       id=session["user_id"], symbol=symbols[i])
-
-    #     quote = lookup(symbols[i])
-    #     rows[0]['price'] = quote['price']
-    #     rows[0]['name'] = quote['name']
-    #     rows[0]['symbol'] = quote['symbol']
-    #     rows[0]['total'] = rows[0]['SUM(shares)']*rows[0]['price']
-    #     if rows[0]['SUM(shares)'] > 0:
-    #         data.append(rows[0])
-    #         grandtotal += rows[0]['total']
-    # print(data)
+   
     return render_template("register.html")
 
 
-# @app.route("/buy", methods=["GET", "POST"])
-# @login_required
-# def buy():
-#     """Buy shares of stock"""
-
-#     if request.method == "POST":
-#         symbol = request.form.get("symbol")
-#         try:
-#             shares = int(request.form.get("shares"))
-#         except ValueError:
-#             return apology("you can only supply an integer", 400)
-#         if not symbol:
-#             return apology("must provide symbol", 400)
-#         quote = lookup(symbol)
-#         if not quote:
-#             return apology("Enter a valid symbol plix", 400)
-#         if not shares:
-#             return apology("must provide shares", 400)
-#         if shares < 1:
-#             return apology("shares should be a positive integer", 400)
-#         price = shares * quote["price"]
-#         rows = db.execute("SELECT cash FROM users WHERE id = :id", id=session["user_id"])
-#         cash = rows[0]['cash']
-
-#         if cash < price:
-#             return apology("Sorry ... You can't afford this", 400)
-
-#         balance = cash - price
-#         symbol = quote['symbol']
-#         db.execute("INSERT INTO tranzact (user_id, symbol, shares, price) VALUES (:user, :symbol, :shares, :price)",
-#                    user=session["user_id"], symbol=symbol, shares=shares, price=quote["price"])
-
-#         db.execute("UPDATE users SET cash=:balance WHERE id=:id", balance=balance, id=session["user_id"])
-
-#         # Redirect user to home page
-#         return redirect("/")
-#     else:
-#         return render_template("buy.html")
 
 
-# @app.route("/check", methods=["GET"])
-# def check():
-#     """Return true if username available, else false, in JSON format"""
-#     username = request.args.get("username")
-#     print(username)
-#     # Query database for username
-#     rows = db.execute("SELECT * FROM users WHERE username = :username",
-#                       username=username)
-#     print(rows)
-#     # Ensure username exists and password is correct
-#     if len(rows) >= 1:
-#         return jsonify(False)
-#     else:
-#         return jsonify(True)
 
-
-# @app.route("/history")
-# @login_required
-# def history():
-#     """Show history of transactions"""
-
-#     rows = db.execute("SELECT symbol, shares, price, time FROM tranzact WHERE user_id=:id ORDER BY time DESC",
-#                       id=session["user_id"])
-#     print(rows)
-#     return render_template("history.html", bigdata=rows)
-
-
-# @app.route("/login", methods=["GET", "POST"])
-# def login():
+@app.route("/login", methods=["GET", "POST"])
+def login():
 #     """Log user in"""
 
 #     # Forget any user_id
-#     session.clear()
+    session.clear()
 
 #     # User reached route via POST (as by submitting a form via POST)
-#     if request.method == "POST":
-#         username = request.form.get("username").strip()
-#         password = request.form.get("password")
+    if request.method == "POST":
+        username = request.form.get("username").strip()
+        password = request.form.get("password")
 #         # Ensure username was submitted
-#         if not username:
-#             return apology("must provide username", 403)
-
+        if not username:
+            return render_template("login.html", msg = "all fields must be filled")
+             
 #         # Ensure password was submitted
-#         elif not password:
-#             return apology("must provide password", 403)
+        elif not password:
+             return render_template("login.html", msg = "all fields must be filled")
 
-#         username = request.form.get("username")
+        # Query database for username
+        rows = db.execute("SELECT * FROM users WHERE username = :username",
+                          username=username)
 
-#         # Query database for username
-#         rows = db.execute("SELECT * FROM users WHERE username = :username",
-#                           username=username)
+        # Ensure username exists and password is correct
+        if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
+            return apology("invalid username and/or password", 403)
+        print
+        # Remember which user has logged in
+        session["user_id"] = rows[0]["id"]
 
-#         # Ensure username exists and password is correct
-#         if len(rows) != 1 or not check_password_hash(rows[0]["hash"], password):
-#             return apology("invalid username and/or password", 403)
-#         print
-#         # Remember which user has logged in
-#         session["user_id"] = rows[0]["id"]
+        # Redirect user to home page
+        return redirect("/")
 
-#         # Redirect user to home page
-#         return redirect("/")
-
-#     # User reached route via GET (as by clicking a link or via redirect)
-#     else:
-#         return render_template("login.html")
+    # User reached route via GET (as by clicking a link or via redirect)
+    else:
+        return render_template("login.html")
 
 
-# @app.route("/logout")
-# def logout():
-#     """Log user out"""
+@app.route("/logout")
+def logout():
+    """Log user out"""
 
-#     # Forget any user_id
-#     session.clear()
+    # Forget any user_id
+    session.clear()
 
-#     # Redirect user to login form
-#     return redirect("/")
+    # Redirect user to login form
+    return redirect("/")
 
 
 
@@ -206,14 +130,14 @@ def register():
         confirmation = request.form.get("confirmation")
 #         # Ensure username was submitted
         if not username:
-            return apology("must provide username", 400)
+            return render_template("register.html", msg = "all fields must be filled")
 
 #         # Ensure password was submitted
         elif not password:
-            return apology("must provide password", 400)
+             return render_template("register.html", msg = "all fields must be filled")
 
         if password != confirmation:
-            return apology("passwords must match", 400)
+             return render_template("register.html", msg = "passwords must match")
 
         else:
             rows = db.execute("SELECT * FROM users WHERE username = :username",
